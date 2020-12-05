81117d68eaaaed581a6ce75d9a560ec44a19fe1c
dieptuthinh <55689660+dieptuthinh@users.noreply.github.com>
2020-12-05 21:16:05 +0700

    update

--- resources/views/partials/content-single.blade.php
+++ resources/views/partials/content-single.blade.php
@@ -1,11 +1,10 @@
 <article @php post_class() @endphp>
     <header class="mb5 pb-8 border-bottom ">
-        <h2>NAME</h2>
         <h1 class="entry-title">{{$project->name}}</h1>
         <a href="#">{{$project->url}}</a>
     </header>
     <div class="entry-content leading-normal">
-        <section class="mb-5 p-5 border-bottom center">
+        <section class="mb-5 p-5 border-bottom" style="display: flex;justify-content: center;align-items: center;">
             <img class="rounded mb-3 img-thumbnail" src="{{$project->img->url}}" alt="{{$project->img->alt}}"
                 title="{{$project->img->title}}" style="width: 500px; height: 500px;">
         </section>
